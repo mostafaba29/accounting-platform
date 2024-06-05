4e6e92f2785fe0f2e0dca5b4f750a5c6ff4c504f
Mostafa Elshahawy <mostafaelshahawy67@gmail.com>
2024-06-05 23:24:10 +0300

    fixed some bugs and typos

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -21,11 +21,8 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-<<<<<<< HEAD
-    secure: false // req.secure || req.headers["x-forwarded-proto"] === "https"
-=======
+    sameSite: "Lax",
     secure: false //req.secure || req.headers["x-forwarded-proto"] === "https"
->>>>>>> df92dca7bcd66de14f6e4ae23650b36362b860a4
   });
 
   // Remove password from output
