4ab06a879e58b3295228fec8c9bd2f6f78894967
mostafaba29 <44523064+mostafaba29@users.noreply.github.com>
2024-08-13 11:54:10 +0300

    added react-query for client/server communications , created a context for the user ,updated navigation bar and fixed the many requests issue

---
+++ frontend/src/lib/api/userApi.ts
@@ -0,0 +1,25 @@
+// 'use server';
+import axios from 'axios';
+
+const userUrl = 'http://localhost:8000/api/v1/users';
+export const fetchUser = async () =>{
+    try {
+        const response = await axios.get(`${userUrl}/me`,
+            {withCredentials: true}
+        );
+        return response.data;
+    }catch(error){
+        throw new Error('failed to fetch user data',error)
+    }
+}
+
+export const userLogout = async ()=>{
+    try {
+        const response = await axios.post(`${userUrl}/logout`,
+            {withCredentials: true}
+        )
+        return {status:response.data.status}
+    }catch(error){
+        throw new Error('failed to logout',error)
+    }
+}
